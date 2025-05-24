c3a81afeb2754ab253b9cfacc32a41bb51b19c14
Jake Thiem <jakethiem@hotmail.com>
2025-05-24 00:31:35 -0500

    feat: float window on top and restore position using WindowAccessor

--- QuestLogHUD/QuestLogHudApp.swift
+++ QuestLogHUD/QuestLogHudApp.swift
@@ -13,7 +13,17 @@ struct QuestLogHUDApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .background(WindowAccessor { window in
+                    if let window = window {
+                        window.title = "Quest Log"
+                        window.level = .floating
+                        window.setFrameAutosaveName("QuestLogHUDWindow")
+                        window.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary]
+                        window.isReleasedWhenClosed = false
+                        window.makeKeyAndOrderFront(nil)
+                        NSApp.activate(ignoringOtherApps: true)
+                    }
+                })
         }
-        .windowStyle(HiddenTitleBarWindowStyle())
     }
 }
